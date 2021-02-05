6b0ae143bc165db790483d87ecd8d8d720c2fb8f
RD42 <42702181+dashr9230@users.noreply.github.com>
2021-02-05 23:41:57 +0100

    Add 0.3.7-R4 archive RSA and TEA key headers
    Maybe for future use?

---
+++ archive/skey_037r4.h
@@ -0,0 +1,15 @@
+//-----------------------------------------
+// SAMP Archive 2 Tool - TEA Keys
+//
+// This file was automatically generated.
+// Do not modify this file!
+//-----------------------------------------
+
+#pragma once
+
+#define TEA_XOR_KEY 170
+
+#define TEA_KEY_SIZE 16
+const BYTE TEA_KEY[] =
+	{0xB9,	0xEA,	0x40,	0x0A,	0xA3,	0x1F,	0x01,	0x23,
+	 0xB0,	0xEA,	0x46,	0x64,	0x78,	0xAF,	0x50,	0x80};

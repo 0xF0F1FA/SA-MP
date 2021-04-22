92f1525731041d48441c173beb7e831cf6466e0c
RD42 <42702181+dashr9230@users.noreply.github.com>
2021-04-22 10:25:55 +0200

    Uppercase SHA256_PassHash results

--- sdk/sha2/sha256.cpp
+++ sdk/sha2/sha256.cpp
@@ -129,6 +129,6 @@ std::string sha256(std::string input)
 	char buf[2*SHA256::DIGEST_SIZE+1];
 	buf[2*SHA256::DIGEST_SIZE] = 0;
 	for (int i = 0; i < SHA256::DIGEST_SIZE; i++)
-		sprintf(buf+i*2, "%02x", digest[i]);
+		sprintf(buf+i*2, "%02X", digest[i]);
 	return std::string(buf);
 }

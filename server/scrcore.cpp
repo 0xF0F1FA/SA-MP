998a1128cb5c227a2cad963d27c6209d2c2b810c
RD42 <42702181+dashr9230@users.noreply.github.com>
2021-08-21 14:54:39 +0200

    FIx SVar and PVar failures when contains characters above 128

--- server/scrcore.cpp
+++ server/scrcore.cpp
@@ -170,7 +170,7 @@ int set_amxstring(AMX *amx,cell amx_addr,const char *source,int max)
   cell* dest = (cell *)(amx->base + (int)(((AMX_HEADER *)amx->base)->dat + amx_addr));
   cell* start = dest;
   while (max--&&*source)
-    *dest++=(cell)*source++;
+    *dest++=(unsigned char)*source++;
   *dest = 0;
   return dest-start;
 }
